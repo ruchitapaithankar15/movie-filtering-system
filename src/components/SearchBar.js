303f83ab9729107cb9b3452be51c1284414b0a8d
ruchitapaithankar15 <ruchitapaithankar@gmail.com>
2024-06-22 20:55:38 -0400

    search filtered

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,30 @@
 // src/components/SearchBar.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './SearchBar.css';
 
 const SearchBar = ({ searchTerm, onSearchChange }) => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        // Fetch data from the JSON file
+        fetch('/movies.json')
+            .then(response => response.json())
+            .then(data => {
+                console.log('Fetched movies:', data);
+                setMovies(data);
+            })
+            .catch(error => console.error('Error fetching the movies:', error));
+    }, []); // Empty dependency array to run only on mount
+
+    useEffect(() => {
+        if (searchTerm) {
+            const filteredMovies = movies.filter(movie =>
+                movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+            );
+            console.log('Filtered movies:', filteredMovies);
+        }
+    }, [searchTerm, movies]);
+
     return (
         <input
             className="search-bar"
